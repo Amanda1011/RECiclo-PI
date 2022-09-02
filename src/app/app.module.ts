ff9fd68f01b1318a7d1c368bad29226c8d6e5d67
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-01 22:54:36 -0300

    Componentes entrar-cadastrar e sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
