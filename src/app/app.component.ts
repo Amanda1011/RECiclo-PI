ddda8942c4c1bb690620c9cf82c0c7cee452bd80
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-09-13 12:49:18 -0300

    Inicio e Menu criado por Gio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reciclo';
+
+  constructor(
+  public auth: AuthService
+
+
+  ){}
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -21,5 +22,14 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
-}
 
+  logado(){
+  let ok = false
+
+  if(environment.token != ''){
+     
+  }
+  return ok
+    }
+
+  }
