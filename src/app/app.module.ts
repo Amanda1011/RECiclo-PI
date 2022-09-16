c4041aab5ca8897f594b190b0aafb6a544de89c6
lorena <dlm.lorena@gmail.com>
2022-09-15 23:40:12 -0300

    orderby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.compon
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { AlertasComponent } from './alertas/alertas.component';
+import { OrderModule } from 'ngx-order-pipe';
 
 
 @NgModule({
@@ -44,7 +45,8 @@ import { AlertasComponent } from './alertas/alertas.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    OrderModule
 
   ],
   providers: [{
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -26,6 +26,9 @@ export class InicioComponent implements OnInit {
   user:User = new User()
   idUser = environment.id
 
+  key = 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService:PostagemService,
