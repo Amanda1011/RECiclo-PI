4e1594cc926893c9db23ecac8d80957c421a68fd
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-20 17:14:36 -0300

    Alterando local

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -26,8 +26,8 @@ export class TemaService {
   }
   
 
-  getByNomeTema(descricao: string): Observable<Tema[]>{
-    return this.http.get<Tema[]>(`hhttps://reciclopi.herokuapp.com/swagger-ui/index.html/tema/descricao/${descricao}`, this.token)
+  getByNomeTema(titulo: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`https://reciclopi.herokuapp.com/swagger-ui/index.html/tema/titulo/${titulo}`, this.token)
   }
 
   postTema(tema: Tema): Observable<Tema>{
